f8611c9d02ec8cedab9673e0ce422953c2d6fd49
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-03-05 15:00:01 +0100

    updates to LLAMADAQ output file structure

--- src/daq2lh5/llama/llama_event_decoder.py
+++ src/daq2lh5/llama/llama_event_decoder.py
@@ -17,10 +17,10 @@ llama_decoded_values_template = {
     # packet index in file
     "packet_id": {"dtype": "uint32"},
     # combined index of FADC and channel
-    "fch_id": {"dtype": "uint32"},
+    "fadc_channel_id": {"dtype": "uint32"},
     # time since epoch
-    "timestamp": {"dtype": "uint64", "units": "clock_ticks"},
-    "status_flag": {"dtype": "uint32"},
+    "time_since_run_start": {"dtype": "float64", "units": "s"},
+    "fadc_status_bits": {"dtype": "uint32"},
     # waveform data --> not always present
     # "waveform": {
     #    "dtype": "uint16",
@@ -69,8 +69,9 @@ class LLAMAEventDecoder(DataDecoder):
         )  # constant part of the t0 of averaged waveforms
 
     def set_channel_configs(self, channel_configs: LLAMA_Channel_Configs_t) -> None:
-        """Receive channel configurations from llama_streamer after header was parsed
-        Adapt self.decoded_values dict based on read configuration
+        """Receive channel configurations from llama_streamer after header was parsed.
+
+        Adapt self.decoded_values dict based on read configuration.
         """
         self.channel_configs = channel_configs
         for fch, config in self.channel_configs.items():
@@ -109,10 +110,11 @@ class LLAMAEventDecoder(DataDecoder):
                 self.__add_energy(self.decoded_values[fch])
 
     def get_key_lists(self) -> list[list[int | str]]:
-        """
-        Return a list of lists of keys available for this decoder.
-        Each inner list are the fch_id's which share the exact same settings (trace lengths, avg mode, ...),
-        so they can end up in the same buffer.
+        """Get list of keys.
+
+        Return a list of lists of keys available for this decoder.  Each inner
+        list are the fadc_channel_id's which share the exact same settings
+        (trace lengths, avg mode, ...), so they can end up in the same buffer.
         """
         if self.channel_configs is None:
             raise RuntimeError(
@@ -125,14 +127,14 @@ class LLAMAEventDecoder(DataDecoder):
             return check_dict_spec_equal(c1, c2, params_for_equality)
 
         kll: list[list[int]] = []  # key-list-list
-        for fch_id, config in self.channel_configs.items():
+        for fadc_channel_id, config in self.channel_configs.items():
             for kl in kll:
                 # use 1st entry of a list of list as "archetype"
                 if check_equal(config, self.channel_configs[kl[0]]):
-                    kl.append(fch_id)
+                    kl.append(fadc_channel_id)
                     break
             else:
-                kll.append([fch_id])
+                kll.append([fadc_channel_id])
         log.debug(f"key lists are: {repr(kll)}")
         return kll
 
@@ -154,66 +156,78 @@ class LLAMAEventDecoder(DataDecoder):
         packet: bytes,
         evt_rbkd: lgdo.Table | dict[int, lgdo.Table],
         packet_id: int,
-        fch_id: int,
+        fadc_channel_id: int,
         # header: lgdo.Table | dict[int, lgdo.Table]
     ) -> bool:
-        """
-        Decodes a single packet, which is a single SIS3316 event, as specified in the Struck manual.
-        A single packet corresponds to a single event and channel, and has a unique timestamp.
-        packets of different channel groups can vary in size!
+        """Decode packet.
+
+        Decodes a single packet, which is a single SIS3316 event, as specified
+        in the Struck manual.  A single packet corresponds to a single event
+        and channel, and has a unique timestamp.  packets of different channel
+        groups can vary in size!
         """
 
-        # Check if this fch_id should be recorded.
-        if fch_id not in evt_rbkd:
-            if fch_id not in self.skipped_channels:
-                self.skipped_channels[fch_id] = 0
-                log.info(f"Skipping channel: {fch_id}")
+        # Check if this fadc_channel_id should be recorded.
+        if fadc_channel_id not in evt_rbkd:
+            if fadc_channel_id not in self.skipped_channels:
+                self.skipped_channels[fadc_channel_id] = 0
+                log.info(f"Skipping channel: {fadc_channel_id}")
                 log.debug(f"evt_rbkd: {evt_rbkd.keys()}")
-            self.skipped_channels[fch_id] += 1
+            self.skipped_channels[fadc_channel_id] += 1
             return False
 
-        tbl = evt_rbkd[fch_id].lgdo
-        ii = evt_rbkd[fch_id].loc
+        tbl = evt_rbkd[fadc_channel_id].lgdo
+        ii = evt_rbkd[fadc_channel_id].loc
 
         # parse the raw event data into numpy arrays of 16 and 32 bit ints
         evt_data_32 = np.frombuffer(packet, dtype=np.uint32)
         evt_data_16 = np.frombuffer(packet, dtype=np.uint16)
 
         # e sti gran binaries non ce li metti
-        # fch_id = (evt_data_32[0] >> 4) & 0x00000fff  --> to be read earlier, since we need size for chopping out the event from the stream
-        timestamp = ((evt_data_32[0] & 0xFFFF0000) << 16) + evt_data_32[1]
+        # fadc_channel_id = (evt_data_32[0] >> 4) & 0x00000fff  --> to be read earlier,
+        # since we need size for chopping out the event from the stream
+        timestamp_in_clock_ticks = ((evt_data_32[0] & 0xFFFF0000) << 16) + evt_data_32[
+            1
+        ]
         format_bits = (evt_data_32[0]) & 0x0000000F
-        tbl["fch_id"].nda[ii] = fch_id
+
+        timestamp = timestamp_in_clock_ticks * self.dt_raw[fadc_channel_id] * 1e-9
+
+        tbl["fadc_channel_id"].nda[ii] = fadc_channel_id
         tbl["packet_id"].nda[ii] = packet_id
-        tbl["timestamp"].nda[ii] = timestamp
+        tbl["time_since_run_start"].nda[ii] = timestamp
+
         offset = 2
         if format_bits & 0x1:
-            tbl["peakHighValue"].nda[ii] = evt_data_16[4]
-            tbl["peakHighIndex"].nda[ii] = evt_data_16[5]
-            tbl["information"].nda[ii] = (evt_data_32[offset + 1] >> 24) & 0xFF
-            tbl["accSum1"].nda[ii] = evt_data_32[offset + 2]
-            tbl["accSum2"].nda[ii] = evt_data_32[offset + 3]
-            tbl["accSum3"].nda[ii] = evt_data_32[offset + 4]
-            tbl["accSum4"].nda[ii] = evt_data_32[offset + 5]
-            tbl["accSum5"].nda[ii] = evt_data_32[offset + 6]
-            tbl["accSum6"].nda[ii] = evt_data_32[offset + 7]
+            tbl["wf_max_sample_value"].nda[ii] = evt_data_16[4]
+            tbl["wf_max_sample_idx"].nda[ii] = evt_data_16[5]
+            tbl["info_bits"].nda[ii] = (evt_data_32[offset + 1] >> 24) & 0xFF
+            tbl["cumsum_1"].nda[ii] = evt_data_32[offset + 2]
+            tbl["cumsum_2"].nda[ii] = evt_data_32[offset + 3]
+            tbl["cumsum_3"].nda[ii] = evt_data_32[offset + 4]
+            tbl["cumsum_4"].nda[ii] = evt_data_32[offset + 5]
+            tbl["cumsum_5"].nda[ii] = evt_data_32[offset + 6]
+            tbl["cumsum_6"].nda[ii] = evt_data_32[offset + 7]
             offset += 7
+
         if format_bits & 0x2:
-            tbl["accSum7"].nda[ii] = evt_data_32[offset + 0]
-            tbl["accSum8"].nda[ii] = evt_data_32[offset + 1]
+            tbl["cumsum_7"].nda[ii] = evt_data_32[offset + 0]
+            tbl["cumsum_8"].nda[ii] = evt_data_32[offset + 1]
             offset += 2
+
         if format_bits & 0x4:
-            tbl["mawMax"].nda[ii] = evt_data_32[offset + 0]
-            tbl["mawBefore"].nda[ii] = evt_data_32[offset + 1]
-            tbl["mawAfter"].nda[ii] = evt_data_32[offset + 2]
+            tbl["maw_max"].nda[ii] = evt_data_32[offset + 0]
+            tbl["maw_before"].nda[ii] = evt_data_32[offset + 1]
+            tbl["maw_after"].nda[ii] = evt_data_32[offset + 2]
             offset += 3
+
         if format_bits & 0x8:
-            tbl["startEnergy"].nda[ii] = evt_data_32[offset + 0]
-            tbl["maxEnergy"].nda[ii] = evt_data_32[offset + 1]
+            tbl["start_energy"].nda[ii] = evt_data_32[offset + 0]
+            tbl["max_energy"].nda[ii] = evt_data_32[offset + 1]
             offset += 2
 
         raw_length_32 = (evt_data_32[offset + 0]) & 0x03FFFFFF
-        tbl["status_flag"].nda[ii] = (
+        tbl["fadc_status_bits"].nda[ii] = (
             (evt_data_32[offset + 0]) & 0x04000000
         ) >> 26  # bit 26
         maw_test_flag = ((evt_data_32[offset + 0]) & 0x08000000) >> 27  # bit 27
@@ -249,35 +263,36 @@ class LLAMAEventDecoder(DataDecoder):
         # error check: waveform size must match expectations
         if raw_length_16 + avg_length_16 != expected_wf_length:
             raise RuntimeError(
-                f"Waveform sizes {raw_length_16} (raw) and {avg_length_16} (avg) doesn't match expected size {expected_wf_length}."
+                f"Waveform sizes {raw_length_16} (raw) and {avg_length_16} (avg) "
+                f"doesn't match expected size {expected_wf_length}."
             )
 
         # store waveform if available:
         if raw_length_16 > 0:
-            tbl["waveform"]["values"].nda[ii] = evt_data_16[
+            tbl["waveform_windowed"]["values"].nda[ii] = evt_data_16[
                 offset * 2 : offset * 2 + raw_length_16
             ]
             offset += raw_length_32
-            tbl["waveform"]["t0"].nda[ii] = self.t0_raw[fch_id]
+            tbl["waveform_windowed"]["t0"].nda[ii] = self.t0_raw[fadc_channel_id]
 
         # store pre-averaged (avg) waveform if available:
         if avg_length_16 > 0:
-            tbl["avgwaveform"]["values"].nda[ii] = evt_data_16[
+            tbl["waveform_presummed"]["values"].nda[ii] = evt_data_16[
                 offset * 2 : offset * 2 + avg_length_16
             ]
             offset += avg_length_32
             # need to update avg waveform t0 based on the offset I get per event
-            tbl["avgwaveform"]["t0"].nda[ii] = (
-                self.t0_avg_const[fch_id]
-                + float(avg_count_status) * self.dt_raw[fch_id]
+            tbl["waveform_presummed"]["t0"].nda[ii] = (
+                self.t0_avg_const[fadc_channel_id]
+                + float(avg_count_status) * self.dt_raw[fadc_channel_id]
             )
 
         if offset != len(evt_data_32):
             raise RuntimeError("I messed up...")
 
-        evt_rbkd[fch_id].loc += 1
+        evt_rbkd[fadc_channel_id].loc += 1
 
-        return evt_rbkd[fch_id].is_full()
+        return evt_rbkd[fadc_channel_id].is_full()
 
     def __add_waveform(
         self,
@@ -286,13 +301,17 @@ class LLAMAEventDecoder(DataDecoder):
         max_samples: int,
         dt: float,
     ) -> None:
-        """
-        Averaged samples are available from the 125 MHz (16 bit) variatnt of the SIS3316 and can be stored independently of raw samples.
-        I use waveform for raw samples (dt from clock itself) and avgwaveform from averaged samples (dt from clock * avg number).
+        """Helper function to add waveform.
 
-        GERDA used to have the low-frequency (waveform) & the high-frequency (aux waveform); here: LF = avgwaveform & HF = waveform.
+        Averaged samples are available from the 125 MHz (16 bit) variatnt of
+        the SIS3316 and can be stored independently of raw samples.  I use
+        waveform for raw samples (dt from clock itself) and waveform_presummed
+        from averaged samples (dt from clock * avg number).
+
+        GERDA used to have the low-frequency (waveform) & the high-frequency
+        (aux waveform); here: LF = waveform_presummed & HF = waveform_windowed.
         """
-        name: str = "avgwaveform" if is_avg else "waveform"
+        name: str = "waveform_presummed" if is_avg else "waveform_windowed"
         decoded_values_fch[name] = {
             "dtype": "uint16",
             "datatype": "waveform",
@@ -304,25 +323,25 @@ class LLAMAEventDecoder(DataDecoder):
         }
 
     def __add_accum1till6(self, decoded_values_fch: dict[str, Any]) -> None:
-        decoded_values_fch["peakHighValue"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["peakHighIndex"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["information"] = {"dtype": "uint32"}
-        decoded_values_fch["accSum1"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["accSum2"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["accSum3"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["accSum4"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["accSum5"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["accSum6"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["wf_max_sample_value"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["wf_max_sample_idx"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["info_bits"] = {"dtype": "uint32"}
+        decoded_values_fch["cumsum_1"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_2"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_3"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_4"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_5"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_6"] = {"dtype": "uint32", "units": "adc"}
 
     def __add_accum7and8(self, decoded_values_fch: dict[str, Any]) -> None:
-        decoded_values_fch["accSum7"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["accSum8"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_7"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["cumsum_8"] = {"dtype": "uint32", "units": "adc"}
 
     def __add_maw(self, decoded_values_fch: dict[str, Any]) -> None:
-        decoded_values_fch["mawMax"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["mawBefore"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["mawAfter"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["maw_max"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["maw_before"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["maw_after"] = {"dtype": "uint32", "units": "adc"}
 
     def __add_energy(self, decoded_values_fch: dict[str, Any]) -> None:
-        decoded_values_fch["startEnergy"] = {"dtype": "uint32", "units": "adc"}
-        decoded_values_fch["maxEnergy"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["start_energy"] = {"dtype": "uint32", "units": "adc"}
+        decoded_values_fch["max_energy"] = {"dtype": "uint32", "units": "adc"}
